710d3ce6c2f5cd5260e390bf93b3ae17ca753d70
nftteaze <t72915318@gmail.com>
2023-11-02 06:14:38 +0000

    title

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -3,7 +3,10 @@ import './AppHeader.css';
 
 const AppHeader = () => (
   <div className="App-header">
-    <h2>DashSnap</h2>
+    <h2 className="title">
+      <span className="dash">Dash</span>
+      <span className="snap">Snap</span>
+    </h2>
   </div>
 );
 
